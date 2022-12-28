c773152dcfc6d5703835fa164e07a3fcdc7f9664
Franco Poffo <franco_sutter@yahoo.com.br>
2022-12-28 11:53:34 -0300

    Corrigindo a formatação dos arquivos

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,9 @@
 const SimpleInput = (props) => {
   return (
     <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" />
       </div>
       <div className="form-actions">
         <button>Submit</button>
